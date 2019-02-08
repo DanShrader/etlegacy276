f575603da8cfd1e50b73ff6ff5a3f870793d75ca
Dan Shrader <Dan@Shraderland.com>
2019-02-08 13:04:21 -0500

    Another try....

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,4 @@
 FROM ubuntu:18.04
 COPY run.sh /
+sudo chmod +x run.sh
 ENTRYPOINT ["/run.sh"]
--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-echo $REPO
-echo $CONGIF
+echo "$REPO"
+echo "$CONGIF"
+
+# docker run --name etlegacy276 danshrader/etlegacy-server-lan276 -e REPO="repo1" -e CONFIG="config123"
